17848f7053d9f3757cf4f5e3959ce30acf00facb
jpaquim <joaopaquim@gmail.com>
2020-02-17 23:32:59 +0000

    Implement new() for non ZSTs
    Use Option<NonNull<T>> instead of Unique<T>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
+use std::mem;
+use std::ptr::{self, NonNull};
+
+pub struct Vec<T> {
+    ptr: Option<NonNull<T>>,
+    cap: usize,
+    len: usize,
+}
+
+impl<T> Vec<T> {
+    pub fn new() -> Self {
+        assert!(mem::size_of::<T>() != 0, "We're not ready to handle ZSTs");
+        Vec {
+            ptr: None,
+            len: 0,
+            cap: 0,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::Vec;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let vec = Vec::<u8>::new();
     }
 }
